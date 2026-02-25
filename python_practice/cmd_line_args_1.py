0c3f1e8bfac0214727dfcd0e39900dbaa0d53f97
Samaran <samaran270s@gmail.com>
2026-02-25 07:32:31 +0530

    Fix argument count display in cmd_line_args_1.py

--- python_practice/cmd_line_args_1.py
+++ python_practice/cmd_line_args_1.py
@@ -3,7 +3,7 @@ import sys
 total_arg = len(sys.argv)
 
 print("Script name:",sys.argv[0])
-print("Number of argument:",len(sys.argv))
+print("Number of argument:",total_arg)
 
 print("Arguments:")
 for i in sys.argv:
