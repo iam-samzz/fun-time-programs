5749bcbc2933a24c41817ca7ca8f2f7c7b2364a1
iam-samzz <samaran270s@gmail.com>
2026-06-25 12:38:18 +0530

    depth of the binary treee using recursion

--- tree/depth_of_the_tree.py
+++ tree/depth_of_the_tree.py
@@ -3,6 +3,9 @@ from level_order_traversal import Node
 
 class Tree:
     def depth(self,root):
+        if root == None:
+            return [0,0]
+
         self.left = 0
         self.right = 0
 
@@ -22,3 +25,25 @@ class Tree:
 
         #this only returns the max length of left branch from the root and the right branch from the root. like , [4,2], here 4 is the length
         return max_list
+
+if __name__ == "__main__":
+    root = Node(5)
+    root.left = Node(12)
+    root.right = Node(13)
+
+    root.left.left = Node(7)
+    root.left.right = Node(14)
+
+    root.right.right = Node(2)
+
+    root.left.left.left = Node(17)
+    root.left.left.right = Node(23)
+
+    root.left.right.left = Node(27)
+    root.left.right.right = Node(3)
+
+    root.right.right.left = Node(8)
+    root.right.right.right = Node(11)
+
+    bt = Tree()
+    print(max(bt.depth(root)))
